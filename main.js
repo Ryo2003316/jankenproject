cc7432b8da9be0af137624035d5c4d6abe4a49e8
Ryo2003316 <140999961+Ryo2003316@users.noreply.github.com>
2023-07-31 21:26:00 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+function janken(r) {
+    
+    // r = 自分の手
+    // c = コンピューターの手
+    // j = 結果
+
+    //0===グー
+    //1===チョキ
+    //2===パー
+
+    let random = Math.random() *100;
+    random = Math.floor(random %3);
+    c=random
+    console.log(random);
+    // 表示する文字列を格納する変数
+    let message = '';
+
+    if (r === 1) {
+        if(c===0){
+            message = 'あなたはグー、コンピューターもグーなのであいこ';
+        }
+        else if(c===1){
+            message = 'あなたはグー、コンピューターはチョキなのであなたの勝ち';
+        }
+        else if(c===2){
+            message = 'あなたはグー、コンピューターはパーなのであなたの負け';
+        }
+    } else if (r === 2) {
+        if(c===0){
+            message = 'あなたはチョキ、コンピューターはグーなのであなたの負け';
+        }
+        else if(c===1){
+            message = 'あなたはチョキ、コンピューターもチョキなのであいこ';
+        }
+        else if(c===2){
+            message = 'あなたはチョキ、コンピューターはパーなのであなたの勝ち';
+        }
+    } else if (r === 3) {
+        if(c===0){
+            message = 'あなたはパー、コンピューターはグーなのであなたの勝ち';
+        }
+        else if(c===1){
+            message = 'あなたはパー、コンピューターはチョキなのであなたの負け';
+        }
+        else if(c===2){
+            message = 'あなたはパー、コンピューターもパーなのであいこ';
+        }
+    }
+    
+    console.log(message);
+
+    const p_hello = document.querySelector('#janken');
+   
+    p_hello.textContent = message;
+
+}
